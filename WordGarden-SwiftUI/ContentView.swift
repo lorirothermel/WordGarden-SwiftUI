e2505ba0e8e69df333a3a69d38afeba70f485c2d
lorirothermel <lori@rothermel1.com>
2024-09-01 12:50:19 -0400

    Chapter 2.3 Working with TextFields

--- WordGarden-SwiftUI/ContentView.swift
+++ WordGarden-SwiftUI/ContentView.swift
@@ -8,17 +8,121 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wordsGuessed = 0
+    @State private var wordsMissed = 0
+    @State private var wordsToGuess = ["SWIFT","DOG","CAT","MOUSE"]
+    @State private var gameStatusMessage = "How Many Guesses to Uncover the Hidden Word?"
+    @State private var currentWord = 0
+    @State private var guessedLetter = ""
+    @State private var imageName = "flower8"
+    @State private var playAgainHidden = true
+    @FocusState private var textFieldIsFocus: Bool
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            
+            HStack {
+                
+                VStack(alignment: .leading) {
+                    Text("Words Guessed: \(wordsGuessed)")
+                    Text("Words Missed: \(wordsMissed)")
+                }  // VStack
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("Words to Guess: \(wordsToGuess.count - wordsGuessed + wordsMissed)")
+                    Text("Words in Game: \(wordsToGuess.count)")
+                }  // VStack
+                
+            }  // HStack
+            .padding(.horizontal)
+            
+            Spacer()
+            
+            Text(gameStatusMessage)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Spacer()
+            
+            // TODO: Switch to wordsToGuess[currentWord]
+                      
+            Text("- - - - -")
+                .font(.title)
+                .fontWeight(.black)
+            
+            
+            if playAgainHidden {
+                
+                HStack {
+                    
+                    TextField("", text: $guessedLetter)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 30)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.gray, lineWidth: 2)
+                        }  // .overlay
+                        .keyboardType(.asciiCapable)
+                        .submitLabel(.done)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.characters)
+                        .onChange(of: guessedLetter) { _, _ in
+                            guessedLetter = guessedLetter.trimmingCharacters(in: .letters.inverted)
+                            
+                            guard let lastChar = guessedLetter.last else {
+                                return
+                            }  // guard let
+                            guessedLetter = String(lastChar).uppercased()
+                        }  // .onChange
+                        .focused($textFieldIsFocus)
+                    
+                    Button("Guess A Letter") {
+                        // TODO: Guess A Letter Button Action Here
+                        textFieldIsFocus = false
+                        
+                    }  // Button
+                    .font(.system(size: 25))
+                    .bold()
+                    .tint(.mint)
+                    .buttonStyle(.borderedProminent)
+                    .padding(.top)
+                    .padding(.bottom)
+                    .disabled(guessedLetter.isEmpty)
+                    
+                }  // HStack
+            } else {
+                
+                Button("Another Word?") {
+                    // TODO: Another Word Button Action Here
+                    
+                }  // Button
+                .font(.system(size: 25))
+                .bold()
+                .buttonBorderShape(.roundedRectangle(radius: 12))
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+            }  // if
+            
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                       
+            
+        }  // VStack
+        .ignoresSafeArea(edges: .bottom)
+        
+        
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
